f8af726c377b5f4063b211c38af73912072eb554
Yanis Bouzelha <bouzelha39@gmail.com>
2022-05-13 13:24:07 +0200

    changing DB connexion string from other AZURE account & deploying the API

---
+++ ApiAzure/Migrations/20220513095946_migrationToOtherAzureAccount.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiAzure.Migrations
+{
+    public partial class migrationToOtherAzureAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
